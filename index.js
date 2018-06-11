cbfd8da4a1a489a7fdc3f941476793b820773a9d
peterdemartini <thepeterdemartini@gmail.com>
2018-06-11 10:29:27 -0700

    upgrade tests to work with the latest version of convict

--- index.js
+++ index.js
@@ -99,32 +99,28 @@ module.exports = (processor) => {
     const validator = require('teraslice/lib/config/validators/config')();
 
     function run(data, extraOpConfig, extraContext) {
-        return process(getProcessor(extraOpConfig, extraContext), data);
+        return processFn(getProcessor(extraOpConfig, extraContext), data);
     }
 
     function runAsync(data, extraOpConfig, extraContext) {
         return Promise.resolve(getProcessor(extraOpConfig, extraContext))
-            .then(proc => process(proc, data));
+            .then(proc => processFn(proc, data));
     }
 
     function runSlices(slices, extraOpConfig, extraContext) {
         const newProcessor = getProcessor(extraOpConfig, extraContext);
-        const results = [];
-        return new Promise((resolve) => {
-            Promise.resolve(slices)
-                .mapSeries(slice => results.push(process(newProcessor, slice)))
-                .then(() => {
-                    // Not yet clear if this is general enough. Trying it out to
-                    // help keep callers simple.
-                    emulateShutdown();
-                })
-                .then(() => {
-                    // Run one last time with empty slice to give processor's
-                    // shutdown logic a chance to flush its state.
-                    results.push(process(newProcessor, []));
-                    resolve(results);
+        return Promise.resolve(slices)
+            .mapSeries(slice => processFn(newProcessor, slice))
+            .then((results) => {
+                // Not yet clear if this is general enough. Trying it out to
+                // help keep callers simple.
+                emulateShutdown();
+
+                return Promise.resolve().then(() => processFn(newProcessor, [])).then((result) => {
+                    results.push(result);
+                    return results;
                 });
-        });
+            });
     }
 
     function getProcessor(opConfig, extraContext) {
@@ -136,13 +132,13 @@ module.exports = (processor) => {
         const jobConfig = validator.validateConfig(jobSchema, jobSpec(opConfig));
 
         return processor.newProcessor(
-            _.merge({}, context, extraContext),
+            _.assign({}, context, extraContext),
             validator.validateConfig(processor.schema(), opConfig),
             jobConfig
         );
     }
 
-    function process(myProcessor, data) {
+    function processFn(myProcessor, data) {
         return myProcessor(data, fakeLogger.logger);
     }
 
@@ -197,6 +193,6 @@ module.exports = (processor) => {
         runSlices,
         emulateShutdown,
         getProcessor,
-        process
+        process: processFn
     };
 };
